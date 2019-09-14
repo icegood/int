1471d7ef5ab1aa04d79a9f0feeb2413f68b511ec
“icegood” <icegood1980@gmail.com>
2019-09-14 17:48:17 +0300

    be able to build either by parts or all

--- cmake_utils/project_specific.cmake
+++ cmake_utils/project_specific.cmake
@@ -1,32 +1,39 @@
-# project specific parameters :
 function(configure_additional_options)
-  include(${CMAKE_CURRENT_SOURCE_DIR}/cmake_utils/${PROJECT_NAME}_options.cmake)
-  configure_file( ./include/${PROJECT_NAME}_options.h.in ${PROJECT_NAME}_options.h @ONLY)
-  target_include_directories(${PROJECT_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/include/${PROJECT_NAME}_options.h.in)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake_utils/${PROJECT_NAME}_options.cmake)
+    configure_file( ./include/${PROJECT_NAME}_options.h.in ${PROJECT_NAME}_options.h @ONLY)
+    target_include_directories(${PROJECT_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+  endif()
 endfunction(configure_additional_options)
 
-# project executable confguration
-function(configure_project_executable)
+function(configure_project_tools)
+  # gdb
+  include(gdb)
+  add_gdb_target()
+  # unit tests
+  enable_testing()
+  include(gunittests)
+  add_tests_target(${PROJECT_NAME})
+  # gprof
+  include(gprof)
+  add_gprof_target(${PROJECT_NAME})
+endfunction(configure_project_tools)
+
+function(configure_project_common)
   set (SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src)
   set (INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
-  
-  set(PROJEXEC_SRCS 
-      ${SOURCE_DIR}/${PROJECT_NAME}.cpp
-  )
-  
-  add_executable(${PROJECT_NAME} ${PROJEXEC_SRCS})
+
   target_include_directories(${PROJECT_NAME} PRIVATE ${INCLUDE_DIR})
   configure_additional_options()
-  
-  target_link_libraries (${PROJECT_NAME} proj_lib)
-  
-  add_gdb_target()
-endfunction(configure_project_executable)
+  if(!hasParent)
+    configure_project_tools(${PROJECT_NAME})
+  endif()
+endfunction(configure_project_common)
 
 # Make sure we tell the topdir CMakeLists that we exist (if build from topdir)
 get_directory_property(hasParent PARENT_DIRECTORY)
 
-if(hasParent)
-  set(PROJECT_${PROJECT_NAME} true PARENT_SCOPE)
-endif()
+#if(hasParent)
+#  set(PROJECT_${PROJECT_NAME} true PARENT_SCOPE)
+#endif()
 
